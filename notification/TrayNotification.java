f8d6d4f9ade7e8db6792471d9ce6768cca875ae1
futbolpal <futbolpal@gmail.com>
2012-07-28 16:08:33 -0400

    up to date with Module implementation
    Signed-off-by: futbolpal <futbolpal@gmail.com>

--- notification/TrayNotification.java
+++ notification/TrayNotification.java
@@ -6,6 +6,7 @@ import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.ImageIcon;
 import javax.swing.JComponent;
 import javax.swing.JPanel;
 import javax.swing.JWindow;
@@ -13,211 +14,209 @@ import javax.swing.Timer;
 import javax.swing.border.BevelBorder;
 import javax.swing.border.CompoundBorder;
 
-import module.Module;
+import module_system.Module;
 import basic_gui.BasicDialog;
 
-public class TrayNotification extends JWindow implements ActionListener, Module
-{
-  private static final int SCREEN_WIDTH = (int) Toolkit.getDefaultToolkit().getScreenSize().getWidth();
+public class TrayNotification extends JWindow implements ActionListener, Module {
+	private static final int SCREEN_WIDTH = (int) Toolkit.getDefaultToolkit()
+			.getScreenSize().getWidth();
 
-  private static final int SCREEN_HEIGHT = (int) Toolkit.getDefaultToolkit().getScreenSize().getHeight();
+	private static final int SCREEN_HEIGHT = (int) Toolkit.getDefaultToolkit()
+			.getScreenSize().getHeight();
 
-  private static final int TIMEOUT = 5;
+	private static final int TIMEOUT = 5;
 
-  private static TrayNotification this_;
+	private static TrayNotification this_;
 
-  private Timer timer_;
+	private Timer timer_;
 
-  private JPanel main_;
+	private JPanel main_;
 
-  private int time_in_;
+	private int time_in_;
 
-  private boolean busy_;
+	private boolean busy_;
 
-  private boolean abort_;
+	private boolean abort_;
 
-  private TrayNotification()
-  {
-    timer_ = new Timer(1000, this);
-    busy_ = false;
-    abort_ = false;
-    time_in_ = 0;
-    main_ = new JPanel();
-    main_.setBorder(new CompoundBorder(new BevelBorder(BevelBorder.RAISED), new BevelBorder(BevelBorder.LOWERED)));
+	private TrayNotification() {
+		timer_ = new Timer(1000, this);
+		busy_ = false;
+		abort_ = false;
+		time_in_ = 0;
+		main_ = new JPanel();
+		main_.setBorder(new CompoundBorder(new BevelBorder(BevelBorder.RAISED),
+				new BevelBorder(BevelBorder.LOWERED)));
 
-    Insets insets = Toolkit.getDefaultToolkit().getScreenInsets(this.getGraphicsConfiguration());
+		Insets insets = Toolkit.getDefaultToolkit().getScreenInsets(
+				this.getGraphicsConfiguration());
 
-    this.requestFocusInWindow();
-    this.add(main_);
-    this.setSize(200, 100);
-    this.setLocation(SCREEN_WIDTH - this.getWidth() - insets.right, SCREEN_HEIGHT - insets.bottom);
+		this.requestFocusInWindow();
+		this.add(main_);
+		this.setSize(200, 100);
+		this.setLocation(SCREEN_WIDTH - this.getWidth() - insets.right,
+				SCREEN_HEIGHT - insets.bottom);
 
-    this.setAlwaysOnTop(true);
-  }
+		this.setAlwaysOnTop(true);
+	}
 
-  public void setVisible(final boolean flag)
-  {
-    if(busy_ && flag)
-    {
-      return;
-    }
-    super.setVisible(true);
+	public void setVisible(final boolean flag) {
+		if (busy_ && flag) {
+			return;
+		}
+		super.setVisible(true);
 
-    // conduct transition
-    new Thread(new Runnable()
-    {
-      private TrayNotification this_ = TrayNotification.this;
+		// conduct transition
+		new Thread(new Runnable() {
+			private TrayNotification this_ = TrayNotification.this;
 
-      public void run()
-      {
-        int direction;
-        int destination;
+			public void run() {
+				int direction;
+				int destination;
 
-        //
-        // declare busy
-        //
-        busy_ = true;
+				//
+				// declare busy
+				//
+				busy_ = true;
 
-        //
-        // declare destination
-        //
-        if(flag)
-        {
-          direction = - 10;
-          destination = SCREEN_HEIGHT - this_.getHeight();
-        }
-        else
-        {
-          destination = SCREEN_HEIGHT;
-          direction = 10;
-        }
+				//
+				// declare destination
+				//
+				if (flag) {
+					direction = -10;
+					destination = SCREEN_HEIGHT - this_.getHeight();
+				} else {
+					destination = SCREEN_HEIGHT;
+					direction = 10;
+				}
 
-        //
-        // do transition
-        //
-        while(this_.getLocation().getY() != destination && ! abort_)
-        {
+				//
+				// do transition
+				//
+				while (this_.getLocation().getY() != destination && !abort_) {
 
-          this_.setLocation((int) this_.getLocation().getX(), (int) this_.getLocation().getY() + direction);
-          try
-          {
-            Thread.sleep(10);
-          }
-          catch(Exception e)
-          {
+					this_.setLocation((int) this_.getLocation().getX(),
+							(int) this_.getLocation().getY() + direction);
+					try {
+						Thread.sleep(10);
+					} catch (Exception e) {
 
-          }
-        }
-        if(flag && !abort_)
-        {
-          timer_.start();
-        }
-        else
-        {
-          cleanUp();
-        }
-      }
-    }).start();
-  }
+					}
+				}
+				if (flag && !abort_) {
+					timer_.start();
+				} else {
+					cleanUp();
+				}
+			}
+		}).start();
+	}
 
+	private synchronized void cleanUp() {
+		this.dispose();
+		main_.removeAll();
+		time_in_ = 0;
+		busy_ = false;
+		abort_ = false;
+	}
 
-  private synchronized void cleanUp()
-  {
-    this.dispose();
-    main_.removeAll();
-    time_in_ = 0;
-    busy_ = false;
-    abort_ = false;
-  }
-  
-  public synchronized void actionPerformed(ActionEvent e)
-  {
-    time_in_ ++ ;
-    if(time_in_ == TIMEOUT)
-    {
-      timer_.stop();
-      this.setVisible(false);
-    }
-  }
+	public synchronized void actionPerformed(ActionEvent e) {
+		time_in_++;
+		if (time_in_ == TIMEOUT) {
+			timer_.stop();
+			this.setVisible(false);
+		}
+	}
 
-  public synchronized void forceInterrupt()
-  {
-    //
-    // if timer is running, display is up, but close prematurely
-    // if timer is not running, display is not up, but may be in transition.
-    //
-    if(timer_.isRunning())
-    {
-      timer_.stop();
-      this.setVisible(false);
-    }
-    else
-    {
-      abort_ = true;
-    }
-  }
+	public synchronized void forceInterrupt() {
+		//
+		// if timer is running, display is up, but close prematurely
+		// if timer is not running, display is not up, but may be in transition.
+		//
+		if (timer_.isRunning()) {
+			timer_.stop();
+			this.setVisible(false);
+		} else {
+			abort_ = true;
+		}
+	}
 
-  public synchronized boolean isBusy()
-  {
-    return busy_;
-  }
+	public synchronized boolean isBusy() {
+		return busy_;
+	}
 
-  public void displayNotification(JComponent p)
-  {
-    if(isBusy())
-    {
-      forceInterrupt();
-    }
-    //
-    // wait.
-    //
-    while(isBusy())
-    {
-      try
-      {
-        Thread.sleep(100);
-      }
-      catch(InterruptedException e)
-      {
-        // TODO Auto-generated catch block
-        e.printStackTrace();
-      }
-    }
-    main_.add(p, BorderLayout.CENTER);
-    this.setVisible(true);
-  }
+	/**
+	 * Creates a notification to be displayed.  
+	 * @param p - The
+	 * component p contains the gui layout to be
+	 * displayed to the user
+	 */
+	public void displayNotification(JComponent p) {
+		if (isBusy()) {
+			forceInterrupt();
+		}
+		//
+		// wait.
+		//
+		while (isBusy()) {
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		main_.add(p, BorderLayout.CENTER);
+		this.setVisible(true);
+	}
 
-  public static TrayNotification instance()
-  {
-    if(this_ == null)
-    {
-      this_ = new TrayNotification();
-    }
-    return this_;
-  }
+	public boolean checkForUpdate() {
+		return false;
+	}
 
-  public boolean checkForUpdate()
-  {
-    return false;
-  }
+	public void update() {
+	}
 
-  public void update()
-  {
-  }
+	public BasicDialog getOptionsDialog() {
+		return null;
+	}
 
-  public BasicDialog getOptionsDialog()
-  {
-    return null;
-  }
+	public String getType() {
+		return "Plugin";
+	}
 
-  public String getType()
-  {
-    return "Plugin";
-  }
+	@Override
+	public void frameworkOpened() {
+		// TODO Auto-generated method stub
 
-  public void close()
-  {
-    System.out.println("CLOSE");
-  }
+	}
 
+	@Override
+	public void frameworkOpening() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public ImageIcon getIcon() {
+		return new ImageIcon("logo.png");
+	}
+
+	/**
+	 * Do nothing on close, so return true to
+	 * indicate we closed successfully
+	 */
+	public boolean close() {
+		return true;
+	}
+
+	/**
+	 * 
+	 * @return TrayNotification handle
+	 */
+	public static TrayNotification instance() {
+		if (this_ == null) {
+			this_ = new TrayNotification();
+		}
+		return this_;
+	}
 }
